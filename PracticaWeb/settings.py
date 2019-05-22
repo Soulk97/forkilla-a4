3aac921daa4c020a873ab862aadcdf012be54f90
David Martin <david.martin2897@gmail.com>
2019-05-22 20:41:56 +0200

    ups

--- PracticaWeb/settings.py
+++ PracticaWeb/settings.py
@@ -138,6 +138,11 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
