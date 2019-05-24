9f22918683aca71196c71d238873ba44169b9908
DAVID MARTIN VILAR <dmartivi35.alumnes@ub052229.mat.ub.edu>
2019-05-24 18:53:57 +0200

    media url removed

--- PracticaWeb/settings.py
+++ PracticaWeb/settings.py
@@ -145,8 +145,6 @@ STATICFILES_DIRS = (
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Login redirect
 LOGIN_URL = '/accounts/login/'
