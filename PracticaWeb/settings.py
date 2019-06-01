4d90361a3a7fe4bd5128986dcb08d1acec3c96f1
David Martin <david.martin2897@gmail.com>
2019-06-01 17:09:33 +0200

    debug true

--- PracticaWeb/settings.py
+++ PracticaWeb/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'h^-(d50!jc=$g4qbw)5gq+hbr0o(1(nt+088-o)i%@=j0p_bnk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
